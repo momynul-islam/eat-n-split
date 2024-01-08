ad362d57d7b0fdbbc8e3ee417adc179f26b1d064
momynul-islam <islammomynul.cod@gmail.com>
2024-01-08 10:01:04 +0600

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import FriendsList from "./FriendsList";
+import Button from "./Button";
+import FormAddFriend from "./FormAddFriend";
+import FormSplitBill from "./FormSplitBill";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  const [friends, setFriends] = useState(initialFriends);
+
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        selectedFriend.id === friend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+    setSelectedFriend(null);
+  }
+
+  function handleSelection(friend) {
+    setSelectedFriend((selected) =>
+      selected?.id === friend.id ? null : friend
+    );
+    setShowAddFriend(false);
+  }
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
+  return (
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+          marginBottom: "20px",
+          fontSize: "40px",
+          color: "tomato",
+          borderBottom: "2px solid #ffa94d",
+        }}
+      >
+        Eat--N--Split
+      </h1>
+
+      <div className="app">
+        <div className="sidebar">
+          <FriendsList
+            friends={friends}
+            selectedFriend={selectedFriend}
+            onSelection={handleSelection}
+          />
+
+          {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+
+          <Button onClick={handleShowAddFriend}>
+            {showAddFriend ? "Close" : "Add Friend"}
+          </Button>
+        </div>
+
+        {selectedFriend && (
+          <div className="sidebar">
+            <FormSplitBill
+              onSplitBill={handleSplitBill}
+              selectedFriend={selectedFriend}
+              key={selectedFriend.id}
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
---
+++ src/components/Friend.js
@@ -0,0 +1,27 @@
+import Button from "./Button";
+
+export default function Friend({ friend, onSelection, selectedFriend }) {
+  return (
+    <li className={selectedFriend?.id === friend.id ? "selected" : ""}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}$
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <Button onClick={() => onSelection(friend)}>
+        {selectedFriend?.id === friend.id ? "Close" : "Select"}
+      </Button>
+    </li>
+  );
+}
